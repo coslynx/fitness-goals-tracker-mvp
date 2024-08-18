1db233036a42a4973e34dd34093917be433aa93f
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-18 23:59:07 +0530

    generated file: `utils/constants.js`

---
+++ utils/constants.js
@@ -0,0 +1,11 @@
+export const GOAL_TYPES = [
+  "Weight Loss",
+  "Muscle Gain",
+  "Distance Running",
+  "Strength Training",
+  "Flexibility",
+];
+
+export const WORKOUT_TYPES = ["Cardio", "Strength", "Flexibility"];
+
+export const INTENSITY_LEVELS = ["Low", "Medium", "High", "Extreme"];
